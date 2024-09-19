adf15e6aa19b9386dca90b6e62dba941800b8d25
rohit <shivam@calibra.live>
2024-09-19 11:24:32 +0530

    changes in settings

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -134,8 +134,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sainrk98@gmail.com'
-EMAIL_HOST_PASSWORD = 'xppt gure wzbs umws'
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'your password'
 
 
-DEFAULT_FROM_EMAIL = 'sainrk98@gmail.com'
+DEFAULT_FROM_EMAIL = 'your email'
